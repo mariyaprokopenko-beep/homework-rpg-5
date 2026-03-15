6e7692eba2d706d506343fcf3782f2086ddbd87f
mary <mariya.prokopenko@narxoz.kz>
2026-03-15 21:14:28 +0500

    Add RewardService

--- src/com/narxoz/rpg/facade/RewardService.java
+++ src/com/narxoz/rpg/facade/RewardService.java
@@ -2,10 +2,25 @@ package com.narxoz.rpg.facade;
 
 public class RewardService {
     public String determineReward(AdventureResult battleResult) {
-        // TODO: Decide reward rules based on battle outcome.
         if (battleResult == null) {
-            return "TODO";
+            return "No reward - invalid battle result";
         }
-        return "TODO";
+
+        String winner = battleResult.getWinner();
+        int rounds = battleResult.getRounds();
+
+        if (winner.equals("Draw")) {
+            return "Participation reward: 50 gold";
+        } else if (winner.contains("Arthas")) {
+            if (rounds < 5) {
+                return "Epic victory! Reward: 500 gold + Legendary item";
+            } else if (rounds < 10) {
+                return "Victory! Reward: 300 gold + Rare item";
+            } else {
+                return "Hard-fought victory! Reward: 200 gold + Common item";
+            }
+        } else {
+            return "Defeat reward: 50 gold";
+        }
     }
 }
